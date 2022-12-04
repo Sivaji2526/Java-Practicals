43c7716c21d41a40d3d4195805f4df8a067b7fb4
Sivaji2526 <119681112+Sivaji2526@users.noreply.github.com>
2022-12-04 13:29:48 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="Hello";
+		String b="world";
+		
+     System.out.println(a +" "+ b);    
+     }
+
+}
